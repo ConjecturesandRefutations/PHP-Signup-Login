8d2d2ccc9838dca4bc2691930b913aeb688cfeef
Alfie Collins <alfiecollins44@gmail.com>
2023-07-29 13:52:53 +0100

    logout and display username

--- index-two.php
+++ index-two.php
@@ -1,7 +1,12 @@
 <?php
-
 session_start();
 
+if (isset($_SESSION["user_id"])) {
+    $mysqli = require __DIR__ . "/database.php";
+    $sql = "SELECT * FROM user WHERE id = {$_SESSION["user_id"]}";
+    $result = $mysqli->query($sql);
+    $user = $result->fetch_assoc();
+}
 ?>
 
 <!DOCTYPE html>
@@ -13,18 +18,12 @@ session_start();
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-
     <h1>Home</h1>
-
     <?php if (isset($_SESSION["user_id"])): ?>
-
-        <p>You are logged in</p>
-
+        <p>Welcome <?= htmlspecialchars($user["name"]) ?></p>
+        <p><a href="logout.php">Log out</a></p>
     <?php else: ?>
-
         <p><a href="login.php">Log in</a> or <a href="signup.php">Sign Up</a></p>
-    
-        <?php endif; ?>
-
-    </body>
-    </html>
+    <?php endif; ?>
+</body>
+</html>
